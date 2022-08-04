73b8160d5bc3c2261ca1254cd5cfc12447971315
NikitaMuhin <mukhin2000070700@gmail.com>
2022-08-04 14:06:46 +0300

    Made refactoring

--- src/main/java/com/orange/web/WebApplication.java
+++ src/main/java/com/orange/web/WebApplication.java
@@ -1,16 +1,12 @@
 package com.orange.web;
 
-import com.orange.web.kafka.SampleProducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class WebApplication {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
 
